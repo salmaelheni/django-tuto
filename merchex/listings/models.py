18a1c655302d03e77a9f9fb685f684e1f88bc177
salmaelheni <sheni6577@gmail.com>
2023-02-06 16:58:25 +0100

    added hometown field to band

---
+++ merchex/listings/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+# Create your models here.
+
+
+class Band(models.Model):
+    def __str__(self):
+        return f'{self.name}'
+    
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+    
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    hometown = models.fields.CharField(max_length=50)
+    
+    
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=255)
+    sold = models.fields.BooleanField(default=True)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)    
+    year = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
---
+++ merchex/listings/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from listings.models import Band
+def hello(request):
+    bands = Band.objects.all()
+    return render(request,'listings/hello.html',context={'bands': bands})
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
